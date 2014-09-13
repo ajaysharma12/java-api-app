fdf0d9f86385bcb54fb57be36e2ee1703b7f76eb
Arunjit Singh <opensrc@ajsd.in>
2014-09-14 00:41:20 +0530

    add some logging

--- src/main/java/in/ajsd/example/GuiceMain.java
+++ src/main/java/in/ajsd/example/GuiceMain.java
@@ -8,11 +8,17 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /** The main servlet context listener that instantiates the Guice {@link Injector}. */
 public class GuiceMain extends GuiceServletContextListener {
 
+  private static final Logger log = LoggerFactory.getLogger(GuiceMain.class);
+
   @Override
   protected Injector getInjector() {
+    log.info("Creating injector");
     return Guice.createInjector(
         new FilterModule(),
         new ApiModule("/api"),
