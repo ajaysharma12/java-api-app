838932d902910b215afb9c96bc50226cf39f669f
Arunjit Singh <opensrc@ajsd.in>
2014-09-20 14:05:26 +0530

    add style and query params

--- gen.py
+++ gen.py
@@ -17,6 +17,39 @@ _template_loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
 _env = jinja2.Environment(loader=_template_loader)
 
 
+def FixOperation(op):
+  query_params = []
+  body_params = []
+  extra_params = []
+  for param in op['parameters']:
+    if param['paramType'] == 'query':
+      query_params.append(param)
+    elif param['paramType'] == 'body':
+      body_params.append(param)
+    else:
+      extra_params.append(param)
+  del op['parameters']
+  if query_params:
+    op['queryParameters'] = query_params
+  if body_params:
+    op['requestBody'] = body_params[0]
+  if extra_params:
+    op['parameters'] = extra_params
+  return op
+
+
+def FixApi(api):
+  for op in api['operations']:
+    FixOperation(op)
+  return api
+
+
+def FixApiDoc(api_doc):
+  for api in api_doc['apis']:
+    FixApi(api)
+  return api_doc
+
+
 def GetService(api_docs_path=None):
   api_docs_path = api_docs_path or FLAGS.api_docs_path
   service = None
@@ -31,7 +64,7 @@ def GetService(api_docs_path=None):
       api = json.loads(f.read())
       if 'models' in api:
         models.append(api.pop('models'))
-      apis.append(api)
+      apis.append(FixApiDoc(api))
 
   service.pop('apis')
   service['apiDocuments'] = apis
@@ -47,7 +80,7 @@ def RenderHtml(service):
 def main(args):
   service = GetService(FLAGS.api_docs_path)
 
-  # print(json.dumps(service))
+  print(json.dumps(service))
 
   docs = RenderHtml(service)
 
