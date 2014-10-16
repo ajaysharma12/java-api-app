4bf8d976b3f4558a6e8abe71ce58e4eeef8bbaab
Arunjit Singh <opensrc@ajsd.in>
2014-10-16 08:37:43 +0530

    fix test script

--- scripts/apitest.py
+++ scripts/apitest.py
@@ -4,19 +4,20 @@ import sys
 import httplib
 import urllib
 
-redirect_url = 'chrome:extension://dfdbebfbdka92ndkn0n2g/token.html?'
+redirect_url = 'chrome:extension://dfdbebfbdka92ndkn0n2g/token.html'
 
 conn = httplib.HTTPConnection('localhost:8080')
 
 params = urllib.urlencode({'u': 'arunjit', 'p': 'pass'})
 headers = {"Content-type": "application/x-www-form-urlencoded"}
-conn.request('POST', '/api/auth?redirect_url=boo', params, headers)
+conn.request('POST', '/api/auth?response_type=token&redirect_uri=boo', params, headers)
 resp = conn.getresponse()
+print resp.read()
+print resp.getheaders()
 if resp.status not in [200, 201, 204]:
-  print resp.read()
   sys.exit(1)
 
-jwt_token = resp.getheader('Location')[4:]
+jwt_token = resp.getheader('Location')[len('boo#token='):]
 print jwt_token
 
 headers = {'Authorization': jwt_token}
